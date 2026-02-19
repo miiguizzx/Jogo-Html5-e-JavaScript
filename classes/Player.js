093c4eef93f9b953d93fb43b9161f16caffed20e
migui <mi.carrica.pereira@gmail.com>
2026-02-19 12:49:53 +0000

    Update Player.js

--- classes/Player.js
+++ classes/Player.js
@@ -46,7 +46,7 @@ class Player {
     getImage(path) {
         /* Cria e devolve uma imagem */
         const image = new Image();
-        image.src = path;
+        image = path;
         return image;
     }
 
